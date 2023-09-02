6a1f73adfd7ba4b56d339a5c9c753d258715d0ee
nagimbsher <49290690+nagimb@users.noreply.github.com>
2023-09-02 20:10:18 +0300

    first commit

---
+++ src/components/featuredProp/FeaturedPop.jsx
@@ -0,0 +1,49 @@
+import "./featuredProp.css"
+
+const FeaturedPop = () => {
+  return (
+    <div className="fp">
+    <div className="fpItem">
+      
+    <img
+     src="https://cf.bstatic.com/xdata/images/hotel/max1024x768/179362750.jpg?k=fd442de52ed044b3eb928255e6c0f3a3499c3885616123982384732064625abb&o=&hp=1" alt=""className="fpImg" />
+    <span className="fpName"> Aparthotel Tell Aviv</span>
+    <span className="fpCity"> Tell Aviv</span>
+    <span className="fpPrice"> Starting from $120</span>
+    <div className="fpRating">
+    <button>8.9</button>
+    <span>Excellent</span>
+    </div>
+    
+    <img src="https://ik.imgkit.net/3vlqs5axxjf/external/ik-seo/https://www.cfmedia.vfmleonardo.com/imageRepo/7/0/106/179/502/L_club_garden_room_1_300_dpi_O/Leonardo-Club-Hotel-Eilat-Room.jpg?tr=w-780%2Ch-437%2Cfo-auto" alt=""className="fpImg" />
+    <span className="fpName"> Aparthotel Leonardo Hotel Jerusalem </span>
+    <span className="fpCity"> Israel</span>
+    <span className="fpPrice"> Starting from $120</span>
+    <div className="fpRating">
+    <button>8.9</button>
+    <span>Excellent</span>
+    </div>
+   
+    <img src="https://cf.bstatic.com/xdata/images/hotel/square600/335868337.webp?k=ce00e66a8909b1ca02d9750cc327cb98288a825292d4774bff5965e842c9f053&o=" alt=""className="fpImg" />
+    <span className="fpName"> Aparthotel Mitzpe Romon</span>
+    <span className="fpCity">  Israel</span>
+    <span className="fpPrice"> Starting from $120</span>
+    <div className="fpRating">
+    <button>8.9</button>
+    <span>Excellent</span>
+    </div>
+    <img src="https://cf.bstatic.com/xdata/images/hotel/square600/469857195.webp?k=61971dc4b6bdc04d9ceada6632328b01eab89eb672bce56115d81d179804f379&o=" alt=""className="fpImg" />
+    <span className="fpName"> Aparthotel Eilot Kibbutz Ktura Eilat</span>
+    <span className="fpCity">  Israel</span>
+    <span className="fpPrice"> Starting from $120</span>
+    <div className="fpRating">
+    <button>8.9</button>
+    <span>Excellent</span>
+    </div>
+    </div>
+    
+    </div>
+  );
+};
+
+export default FeaturedPop
